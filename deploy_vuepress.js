15f122cff6f41b0675dd76af066e300957c2e94f
canwdev <c2097@live.com>
2019-10-08 15:01:42 +0800

    优化部署逻辑

--- deploy_vuepress.js
+++ deploy_vuepress.js
@@ -2,18 +2,17 @@ const sh = require('shelljs')
 // const open = require('open')
 const path = require('path')
 
+const utils = require('./utils.js')
+const cd = utils.cd
+const exec = utils.exec
+
 if (!sh.which('git')) {
   sh.echo('Sorry, this script requires git');
   sh.exit(1);
 }
 
 const projectDir = path.join(__dirname, 'projects', 'notes-vuepress')
-const productionDir = path.join(__dirname, 'projects', 'canwdev.gitee.io')
-
-function cd(dir, tip) {
-  if (sh.cd(dir).code === 1) sh.exit(1)
-  console.log(tip||'cd', sh.pwd().toString())
-}
+const productionGit = 'git@gitee.com:canwdev/canwdev.git' // 将被覆盖，请勿填错！
 
 cd(projectDir, '工程目录')
 
@@ -26,17 +25,13 @@ if (sh.exec('yarn install').code === 1) sh.exit(1)
 console.log('>>> 开始构建')
 if (sh.exec('npm run build').code === 1) sh.exit(1)
 
-console.log('>>> 删除之前版本')
-cd(productionDir)
-if (sh.rm('-r', ...sh.ls(productionDir)).code === 1) sh.exit(1)
-
-console.log('>>> 复制文件')
-cd(projectDir)
-if (sh.cp('-R', './docs/.vuepress/dist/*', productionDir).code === 1) sh.exit(1)
-
 console.log('>>> 提交修改')
-cd(productionDir)
-if (sh.exec('git add . && git commit -m "update" && git push').code === 1) sh.exit(1)
+cd('docs/.vuepress/dist', 'dist目录')
+// 创建新git项目并强制推送到pages项目（覆盖）
+exec('git init && git add -A && git commit -m "deploy"')
+exec(`git push -f ${productionGit} master`)
 
 console.log('>>> VuePress 部署成功！')
+
+// cd('-') // 返回之前目录
 // open('https://gitee.com/canwdev/canwdev/pages')
